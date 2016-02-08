ff6e37f7ec842fffe532a9835ca930775487727f
Anderson Souza <jair_anderson_bs@hotmail.com>
2016-02-08 03:43:10 -0300

    usando métodos assincronos

--- src/io/github/jass2125/sessionsbeans/LancadorDeDado.java
+++ src/io/github/jass2125/sessionsbeans/LancadorDeDado.java
@@ -1,7 +1,12 @@
 package io.github.jass2125.sessionsbeans;
 
+import java.util.Map;
+import java.util.concurrent.Future;
+
 public interface LancadorDeDado {
 
 	public int lanca();
+	
+	public Future<Map<Integer, Integer>> calculaFrequencia();
 	
 }
--- src/io/github/jass2125/sessionsbeans/LancadorDeDadoBean.java
+++ src/io/github/jass2125/sessionsbeans/LancadorDeDadoBean.java
@@ -1,9 +1,13 @@
 package io.github.jass2125.sessionsbeans;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.Future;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.AsyncResult;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
@@ -31,5 +35,25 @@ public class LancadorDeDadoBean implements LancadorDeDado {
 			LancadorDeDadoBean.cont--;
 			System.out.println("Total: " + LancadorDeDadoBean.cont);
 		}
+	}
+	public Future<Map<Integer, Integer>> calculaFrequencia(){
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		map.put(1, 0);
+		map.put(2, 0);
+		map.put(3, 0);
+		map.put(4, 0);
+		map.put(5, 0);
+		map.put(6, 0);
+		
+		for (int i = 0; i < 500; i++) {
+			int v = this.gerador.nextInt(6) + 1;
+			map.put(v, map.get(v) + 1);
+			try{
+				Thread.sleep(100);
+			}catch(InterruptedException e){}
+			System.out.println(i);
+		}
+		
+		return new AsyncResult<Map<Integer,Integer>>(map);
 	}
 }
